362cfaa4cf68c88fc0c00add4d752bcaab98d97a
rawan6922&9 <rawanh.abuhajer@gmail.com>
2023-04-26 19:38:25 +0300

    add task

---
+++ task.js
@@ -0,0 +1,107 @@
+let age= 27
+if (age>18)
+console.log("You are an adult");
+
+let score= 45
+if (score <= 50)
+console.log("You failed the test");
+
+let name= "john"
+if (name != "john")
+console.log("Hello, John");
+
+let day= "Monday"
+if (name != "saturday & sunday")
+console.log("its weekday");
+
+let num= "4"
+if (num/2 == 2)
+console.log("the number is even");
+
+let char= "a"
+console.log(/a/.test(char));
+
+let list = [1,2,3]
+if (!Array.isArray(list))
+then;
+console.log(`its an array`);
+
+let x = 5
+if ( x > 0) 
+console.log("x is a positive number");
+
+let y = -5
+if ( y < 0) 
+console.log("x is a negative number");
+
+let  z = 9
+if (z/3 == 3)
+console.log("z is a multiple of 3");
+
+let gpa = 3.5
+if (gpa >= 3.0)
+console.log("Congratulations, you have a good GPA!");
+
+let password = "mypassword123"
+if( password.length >=8 )
+console.log("Your password is strong");
+
+let age1 = 30
+if( age1 >18 & age1<65)
+console.log("You are of working age");
+
+let color = "red"
+if (color =="red" || color=="green" || color=="blue")
+console.log("color is a primary color");
+
+let number1 = 11
+let number2 = "19"
+let number3 = "xyz"
+let number4 = "17.5"
+let number5 = "21F"
+
+if(isNaN(number1)){
+	console.log(number1 + " is not a valid number ");} 
+    else{
+    console.log(number1 + " is a valid number ");
+   }
+
+if(isNaN(number2)){
+	console.log(number2 + " is not a valid number ");} 
+    else{
+    console.log(number2 + " is a valid number ");
+   }
+
+if(isNaN(number3)){
+	console.log(number3 + " is not a valid number ");} 
+    else{
+    console.log(number3 + " is a valid number ");
+   }
+
+if(isNaN(number4)){
+	console.log(number4 + " is not a valid number ");} 
+    else{
+    console.log(number4 + " is a valid number ");
+   }
+
+if(isNaN(number5)){
+	console.log(number5 + " is not a valid number ");} 
+    else{
+    console.log(number5 + " is a valid number ");
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
